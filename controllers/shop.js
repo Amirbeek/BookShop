7b153b65aa80ac0ee98309b6af9b3d18eaf6d936
Amirbek <124478211+Amirbek-King@users.noreply.github.com>
2024-10-25 22:12:03 +0100

    Dynamic pagination Buttons

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,7 +4,7 @@ const pdfkit = require('pdfkit')
 const Product = require('../models/product');
 const Order = require('../models/order');
 const PDFDocument = require("pdfkit");
-
+const ITEMS_PER_PAGE = 2
 exports.getProducts = (req, res, next) => {
     Product.find()
         .then(products => {
@@ -40,12 +40,26 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-    Product.find()
+        const page = +req.query.page || 1;
+        let totalItems = 0;
+        Product.find()
+            .countDocuments().then(numProduct=>{
+            totalItems = numProduct
+            return Product.find()
+                .skip((page - 1)*ITEMS_PER_PAGE)
+                .limit(ITEMS_PER_PAGE)
+        })
         .then(products => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
-                path: '/'
+                path: '/',
+                currentPage: page,
+                hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+                hasPreviousPage:page>1,
+                nextPage: page+1,
+                previousPage: page -1,
+                lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
             });
         })
         .catch(err => {
