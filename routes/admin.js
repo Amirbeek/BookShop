b2da054b09795d8aa8f580a3edf8c81fa7cffc74
Amirbek <124478211+Amirbek-King@users.noreply.github.com>
2024-10-28 23:11:37 +0000

    sending and handling backround Request

--- routes/admin.js
+++ routes/admin.js
@@ -31,6 +31,6 @@ const router = express.Router();
         body('description').isLength({min:5 ,max :600}).trim()
     ],isAuth, adminController.postEditProduct);
 
-    router.post('/delete-product',isAuth,  adminController.postDeleteProduct);
+    router.delete('/product/:productId',isAuth,  adminController.deleteProduct);
 
     module.exports = router;
